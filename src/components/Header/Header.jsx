a794fc76a94090376d3fed3b665c5129586d83e7
Yuliya Zozulya <djulia@ukr.net>
2023-05-27 18:20:19 +0300

    adding svg

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,15 @@
 import { NavLink, useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { useMediaQuery } from 'react-responsive';
 import UserInfo from 'components/UserInfo/UserInfo';
 import { getLogin } from 'redux/auth/auth-selectors';
 
 import s from './Header.module.scss';
 import Logo from 'components/Shared/Logo';
+import search from '../../images/header/search-icon.svg';
 
 const Header = () => {
+  const isDesctop = useMediaQuery({ minWidth: 1280 });
   const location = useLocation();
   const getClassName = ({ isActive }) => {
     return isActive ? `${s.link} ${s.active}` : s.link;
@@ -18,7 +21,20 @@ const Header = () => {
     <header className={s.header}>
       <div className={s.containerTop}>
         <Logo />
-        <UserInfo />
+        {!isDesctop && (
+          <div>
+            <svg
+              className="s.icon-search"
+              width="16"
+              height="16"
+              viewBox="0 0 16 16"
+            >
+              <use href="../../images/header/search-icon.svg#icon-search"></use>
+            </svg>
+            {/* <img src={search} alt="search icon" width="16" height="16" /> */}
+          </div>
+        )}
+        {isDesctop && <UserInfo />}
       </div>
       {isLogin && (
         <div className={s.containerBottom}>
