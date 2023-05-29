5918527b76dd404568ab3ce1374675ed752b1845
Kateryna Kleshch <ketrins1993@gmail.com>
2023-05-29 23:19:41 +0300

    add styles

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -39,40 +39,42 @@ const Login = () => {
   return (
     <section className={s.login}>
       <Container>
-        <h2 className={s.title}>Вхід</h2>
-        <form className={s.form} onSubmit={handleSubmit(onSubmit)}>
-          <Controller
-            control={control}
-            name="email"
-            rules={{
-              required: true,
-            }}
-            render={({ field: { onChange, value } }) => (
-              <TextField
-                value={value}
-                control={control}
-                handleChange={onChange}
-                {...field.email}
-              />
-            )}
-          />
-          <Controller
-            control={control}
-            name="password"
-            rules={{ required: true, minLength: 8, maxLength: 30 }}
-            render={({ field: { onChange, value } }) => (
-              <TextField
-                value={value}
-                control={control}
-                handleChange={onChange}
-                {...field.password}
-              />
-            )}
-          />
-          <div className={s.wrap}>
-            <Button text="Ввійти" btnClass="btnLight" />
-          </div>
-        </form>
+        <div className={s.box}>
+          <h2 className={s.title}>Вхід</h2>
+          <form className={s.form} onSubmit={handleSubmit(onSubmit)}>
+            <Controller
+              control={control}
+              name="email"
+              rules={{
+                required: true,
+              }}
+              render={({ field: { onChange, value } }) => (
+                <TextField
+                  value={value}
+                  control={control}
+                  handleChange={onChange}
+                  {...field.email}
+                />
+              )}
+            />
+            <Controller
+              control={control}
+              name="password"
+              rules={{ required: true, minLength: 8, maxLength: 30 }}
+              render={({ field: { onChange, value } }) => (
+                <TextField
+                  value={value}
+                  control={control}
+                  handleChange={onChange}
+                  {...field.password}
+                />
+              )}
+            />
+            <div className={s.wrap}>
+              <Button text="Ввійти" btnClass="btnLight" />
+            </div>
+          </form>
+        </div>
       </Container>
     </section>
   );
