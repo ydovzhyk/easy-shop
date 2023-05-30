fad3fe34c0b49f9bb45d4367bc148cc56e44a9a6
Kateryna Kleshch <ketrins1993@gmail.com>
2023-05-31 00:49:04 +0300

    add google button

--- src/components/AuthWrapper/AuthWrapper.jsx
+++ src/components/AuthWrapper/AuthWrapper.jsx
@@ -1,12 +1,14 @@
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { FcGoogle } from 'react-icons/fc';
 
 import s from './AuthWrapper.module.scss';
 
 import Container from 'components/Shared/Container';
 
 import { getError, getNewUserId } from 'redux/auth/auth-selectors';
+import Text from 'components/Shared/Text/Text';
 // import Register from 'components/Register/Register';
 // import Login from 'components/Login/Login';
 
@@ -17,6 +19,10 @@ const AuthWrapper = () => {
 
   const location = useLocation();
   console.log(location);
+  const googleText =
+    location.pathname === '/login'
+      ? 'Увійти швидко з Google'
+      : 'Зареєструватись швидко з Google';
 
   if (!errorLogin && newUserId) {
     return <Navigate to="/login" />;
@@ -34,6 +40,14 @@ const AuthWrapper = () => {
               <h2 className={s.title}>Реєстрація</h2>
             </Link>
           </div>
+          <Text textClass="google-text" text={googleText} />
+          <a
+            href="https://ydovzhyk.github.io/easy-shop/"
+            className={s.googleBtn}
+          >
+            <FcGoogle size={24} />
+            Google
+          </a>
           {/* {location.pathname === '/registration' && <Register />} */}
           {/* {location.pathname === '/login' && <Login />} */}
           <Outlet />
