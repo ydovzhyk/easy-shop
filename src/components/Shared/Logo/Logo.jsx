3ed1e2c70095f47bf2140bdb07fac07960f9534c
Yuliya Zozulya <djulia@ukr.net>
2023-05-31 21:20:52 +0300

    fixing logo

--- src/components/Shared/Logo/Logo.jsx
+++ src/components/Shared/Logo/Logo.jsx
@@ -1,42 +1,15 @@
 import { Link } from 'react-router-dom';
-import { useMediaQuery } from 'react-responsive';
 
-// import ld from '../../../images/logo/logo.svg';
 import s from './Logo.module.scss';
 
 const Logo = () => {
-  const isDesctop = useMediaQuery({ minWidth: 1280 });
-  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1279 });
-  const isMobile = useMediaQuery({ maxWidth: 767 });
-
   return (
     <>
-      {isMobile && (
+      <div className={s.logoBox}>
         <Link to="/" className={s.logo}>
           EASYshop
-          {/* <div className={s.logoBackground}>
-            <img src={ld} alt="web-site logo" width="95" />
-          </div> */}
         </Link>
-      )}
-
-      {isTablet && (
-        <Link to="/" className={s.logo}>
-          EASYshop
-          {/* <div className={s.logoBackground}> */}
-          {/* <img src={ld} alt="web-site logo" width="115" /> */}
-          {/* </div> */}
-        </Link>
-      )}
-
-      {isDesctop && (
-        <Link to="/" className={s.logo}>
-          {/* <h1 className={s.logoBackground}> */}
-          EASYshop
-          {/* <img src={ld} alt="web-site logo" width="115" /> */}
-          {/* </h1> */}
-        </Link>
-      )}
+      </div>
     </>
   );
 };
