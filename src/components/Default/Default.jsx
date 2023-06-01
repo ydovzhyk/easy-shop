c9cab076289e2fc4316441583d538ce60234d8a8
Kateryna Kleshch <ketrins1993@gmail.com>
2023-06-01 16:01:32 +0300

    start making slider

--- src/components/Default/Default.jsx
+++ src/components/Default/Default.jsx
@@ -3,17 +3,20 @@ import { getLogin } from 'redux/auth/auth-selectors';
 import SiteStatistic from 'components/SiteStatistic/SiteStatistic';
 
 import s from './Default.module.scss';
+import Slider from 'components/Slider/Slider';
 
 const Default = () => {
   const isUserLogin = useSelector(getLogin);
 
   return (
     <section className={s.default}>
+      <Slider />
       {!isUserLogin && (
         <div className={s.defaultTitle}>
           <h2 className={s.title}>Перш ніж почати зареєструйтеся!</h2>
         </div>
       )}
+
       {isUserLogin && (
         <>
           <SiteStatistic />
---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,95 @@
+import ImageGallery from 'react-image-gallery';
+import React from 'react';
+// import Container from 'components/Shared/Container';
+import s from './Slider.module.scss';
+import imgs1 from '../../images/slider/client.jpg';
+import imgs2 from '../../images/slider/kids.jpg';
+import imgs3 from '../../images/slider/cosmetics.jpg';
+import imgs4 from '../../images/slider/store.jpg';
+import imgs5 from '../../images/slider/man.jpg';
+import imgs6 from '../../images/slider/beauty.jpg';
+import imgs7 from '../../images/slider/men.jpg';
+import imgs8 from '../../images/slider/women.jpg';
+import imgs9 from '../../images/slider/shop-maneken.jpg';
+import imgs10 from '../../images/slider/t-shirt.jpg';
+
+const images = [
+  {
+    original: imgs1,
+    thumbnail: imgs1,
+    originalClass: [s.image],
+  },
+  {
+    original: imgs2,
+    thumbnail: imgs2,
+    originalClass: [s.image],
+  },
+  {
+    original: imgs3,
+    thumbnail: imgs3,
+    originalClass: [s.image],
+  },
+  {
+    original: imgs4,
+    thumbnail: imgs4,
+    originalClass: [s.image],
+  },
+  {
+    original: imgs5,
+    thumbnail: imgs5,
+    originalClass: [s.image],
+  },
+  {
+    original: imgs6,
+    thumbnail: imgs6,
+    originalClass: [s.image],
+  },
+  {
+    original: imgs7,
+    thumbnail: imgs7,
+    originalClass: [s.image],
+  },
+  {
+    original: imgs8,
+    thumbnail: imgs8,
+    originalClass: [s.image],
+  },
+  {
+    original: imgs9,
+    thumbnail: imgs9,
+    originalClass: [s.image],
+  },
+  {
+    original: imgs10,
+    thumbnail: imgs10,
+    originalClass: [s.image],
+  },
+];
+
+const array = ['client.jpg', 'kids.jpg'];
+array.map(item => ({
+  original: `../../images/slider/${item}`,
+  thumbnail: `../../images/slider/${item}`,
+  originalClass: [s.image],
+}));
+
+const options = {
+  lazyLoad: true,
+  showThumbnails: false,
+  showFullscreenButton: false,
+  showBullets: true,
+  showPlayButton: false,
+  //   autoPlay: true,
+  slideInterval: 4000,
+  additionalClass: [s.gallery],
+};
+
+const Slider = () => {
+    return (
+      // <Container>
+      <ImageGallery items={images} {...options} className={s.gallery} />
+      // </Container>
+    );
+};
+
+export default Slider;
