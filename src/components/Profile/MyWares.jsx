a18b898f66c38254a51924e27b5db33e3d3f04bb
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2023-06-08 16:15:05 +0300

    Update MyWares.jsx

--- src/components/Profile/MyWares.jsx
+++ src/components/Profile/MyWares.jsx
@@ -1,4 +1,16 @@
+import { useSelector, useDispatch } from 'react-redux';
+import {getUserProducts } from 'redux/product/product-operations';
+import { getProducts } from 'redux/product/product-selectors';
+import { getID } from 'redux/auth/auth-selectors';
+
 const MyWares = () => {
+    const dispatch = useDispatch();
+    const products = useSelector(getProducts);
+    const userID = useSelector(getID);
+    console.log(products);
+
+    dispatch(getUserProducts(userID));
+    
     return (
         <h3>My wares</h3>
     )
