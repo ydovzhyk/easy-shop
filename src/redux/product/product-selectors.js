e54cef44a4a0ffdb56d1d1444b53f576f4810241
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2023-06-09 15:44:24 +0300

    Update product-selectors.js

--- src/redux/product/product-selectors.js
+++ src/redux/product/product-selectors.js
@@ -1,2 +1,3 @@
 export const getProducts = ({ products }) => products.allProducts;
 export const getMessage = ({ products }) => products.message;
+export const getMyProducts = ({ products }) => products.userProducts;
