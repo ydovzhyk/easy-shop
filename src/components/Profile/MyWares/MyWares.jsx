d7ea77758594d26336ebdc48e6b4f801e244d2e0
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2023-06-14 17:19:44 +0300

    Update MyWares.jsx

--- src/components/Profile/MyWares/MyWares.jsx
+++ src/components/Profile/MyWares/MyWares.jsx
@@ -8,7 +8,6 @@ import {
   // getProducts,
   getMyProducts,
 } from 'redux/product/product-selectors';
-import Container from 'components/Shared/Container';
 import NoPhoto from 'images/catalog_photo/no_photo.jpg';
 import Text from 'components/Shared/Text/Text';
 import Button from 'components/Shared/Button/Button';
@@ -25,34 +24,59 @@ const myProducts = useSelector(getMyProducts);
 console.log('myProducts in MyWares', myProducts);
 
 return (
-    <Container>
+    <div>
+    
         <section className={s.myWaresWrapper}>
             <ul className={s.waresList}>
-                {myProducts.map(({ _id, mainPhotoUrl, nameProduct, price }) => (
+                {myProducts.map(({ _id, mainPhotoUrl, additionalPhotoUrl, nameProduct, price }) => (
                     <li className={s.wareItem} key={_id}>
-                        <div className={s.partWrapper}>
+                        <div className={s.partPhotoWrapper}>
+
                             <img
                                 className={s.photoCard}
                                 src={mainPhotoUrl}
                                 onError={e => (e.target.src = NoPhoto)}
                                 alt={nameProduct}
                             />
+                            <ul className={s.additionalPhotoWrapper}>
+                                {additionalPhotoUrl.map(photo => (
+                                    <li className={s.additionalPhoto} key={photo.index}>
+                                        <img
+                                            className={s.additionalPhotoCard}
+                                            src={photo}
+                                            onError={e => (e.target.src = NoPhoto)}
+                                            alt={nameProduct}
+                                        />
+                                    </li>
+                                )
+                                    
+                                )}
+
+                            </ul>
                         </div>
-                        <div className={s.partWrapper}>
-                            <Text textClass="titleGroupItems" text={nameProduct} />
+                        <div className={s.descriptionWrapper}>
+                            <Text textClass="after-title" text={nameProduct} />
                             <Text textClass="after-title" text={`${price}грн.`} />
                         </div>
-                        <div className={s.partWrapper}>
-                            <Button btnClass="btnLight" text="Відгуки" />
-                            <Button btnClass="btnLight" text="Змінити" />
-                            <Button btnClass="btnLight" text="Видалити" />
+                        <div className={s.buttonWrapper}>
+                            <Button btnClass="myWareButton" text="Відгуки" />
+                            <Button btnClass="myWareButton" text="Змінити" />
+                            <Button btnClass="myWareButton" text="Видалити" />
                         </div>
                     </li>
                 ))}
             </ul>
-            <Button btnClass="btnLight" text="Завантажити ще"/>
-        </section>
-    </Container>
+            {myProducts.length > 0 &&
+                <>
+                <Button btnClass="btnLight" text="Завантажити ще" />
+                <Button btnClass="btnLight" text="На початок"/>
+                </>
+                
+            }
+            
+            </section>
+            
+    </div>
 );
 };
 
