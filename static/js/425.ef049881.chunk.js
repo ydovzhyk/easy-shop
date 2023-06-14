2ead477db6ca6c10ad159ba3b45cabaeab94fe5f
ydovzhyk <48929257+ydovzhyk@users.noreply.github.com>
2023-06-14 09:45:42 +0000

    Deploying to gh-pages from @ ydovzhyk/easy-shop@ef7d8102a33da740800ddb802b71cb451fc6c839 🚀

---
+++ static/js/425.ef049881.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkeasy_shop=self.webpackChunkeasy_shop||[]).push([[425],{7425:function(t,a,n){n.d(a,{gcy:function(){return h}});var e=n(9983);function h(t){return(0,e.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"g",attr:{},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M12 10.828l-4.95 4.95-1.414-1.414L12 8l6.364 6.364-1.414 1.414z"}}]}]})(t)}}}]);
+//# sourceMappingURL=425.ef049881.chunk.js.map
