597b80f7a2ae333eb619d4f28f469ee99f082a87
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2023-06-15 14:19:35 +0300

    Update MyWares.jsx

--- src/components/Profile/MyWares/MyWares.jsx
+++ src/components/Profile/MyWares/MyWares.jsx
@@ -2,11 +2,11 @@ import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getUserProducts } from 'redux/product/product-operations';
 import {
-  getMyProductsPages,
   getMyProducts,
+  getMyProductsTotal,
+  getMyProductsPages,
 } from 'redux/product/product-selectors';
 import { clearUserProducts } from 'redux/product/product-slice';
-import Container from 'components/Shared/Container/Container';
 import NoPhoto from 'images/catalog_photo/no_photo.jpg';
 import Text from 'components/Shared/Text/Text';
 import Button from 'components/Shared/Button/Button';
@@ -16,6 +16,7 @@ const MyWares = () => {
   const dispatch = useDispatch();
   const [currentPage, setCurrentPage] = useState(1);
   const myProducts = useSelector(getMyProducts);
+  console.log('myProducts:', myProducts);
   const myProductsTotalPages = useSelector(getMyProductsPages);
 
   useEffect(() => {
@@ -26,46 +27,66 @@ const MyWares = () => {
   useEffect(() => {
     dispatch(getUserProducts(currentPage));
   }, [dispatch, currentPage]);
+  console.log('myProductsTotalPages:', myProductsTotalPages);
+  const myProductsTotal = useSelector(getMyProductsTotal);
+  console.log('myProductsTotal:', myProductsTotal);
+  
+  console.log('currentPage', currentPage);
 
   const handleLoadMore = async () => {
     setCurrentPage(currentPage + 1);
   };
 
   return (
-    <Container>
+    <div>
       <section className={s.myWaresWrapper}>
         <ul className={s.waresList}>
-          {myProducts.map(({ _id, mainPhotoUrl, nameProduct, price }) => (
+          {myProducts.map(({ _id, mainPhotoUrl, additionalPhotoUrl, nameProduct, description, price }) => (
             <li className={s.wareItem} key={_id}>
-              <div className={s.partWrapper}>
+              <div className={s.partPhotoWrapper}>
                 <img
                   className={s.photoCard}
                   src={mainPhotoUrl}
                   onError={e => (e.target.src = NoPhoto)}
                   alt={nameProduct}
                 />
+                <ul className={s.additionalPhotoWrapper}>
+                  {additionalPhotoUrl.map((photo, index) => (
+                    <li className={s.additionalPhoto} key={index}>
+                      <img
+                        className={s.additionalPhotoCard}
+                        src={photo}
+                        onError={e => (e.target.src = NoPhoto)}
+                        alt={nameProduct}
+                      />
+                    </li>
+                  ))}
+                </ul>
               </div>
               <div className={s.partWrapper}>
-                <Text textClass="titleGroupItems" text={nameProduct} />
+                <Text textClass="after-title" text={nameProduct} />
                 <Text textClass="after-title" text={`${price}грн.`} />
               </div>
-              <div className={s.partWrapper}>
-                <Button btnClass="btnLight" text="Відгуки" />
-                <Button btnClass="btnLight" text="Змінити" />
-                <Button btnClass="btnLight" text="Видалити" />
+              <div className={s.buttonWrapper}>
+                <Button btnClass="myWareButton" text="Відгуки" />
+                <Button btnClass="myWareButton" text="Змінити" />
+                <Button btnClass="myWareButton" text="Видалити" />
               </div>
             </li>
           ))}
         </ul>
         {currentPage < myProductsTotalPages && (
-          <Button
+          <>
+            <Button
             btnClass="btnLight"
             text="Завантажити ще"
             handleClick={handleLoadMore}
-          />
+            />
+            <Button btnClass="btnLight" text="На початок"/>
+          </>
         )}
       </section>
-    </Container>
+    </div>
   );
 };
 
