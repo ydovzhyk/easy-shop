27f9f2a832e3751e1f7c35087400c610e2935085
Yuliya Zozulya <djulia@ukr.net>
2023-06-16 15:13:28 +0300

    template of producte render

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
 // import { useDispatch } from 'react-redux';
 // import { useSearchParams } from 'react-router-dom';
 // import { searchProducts } from 'redux/product/product-operations';
@@ -13,65 +13,68 @@ import s from './Products.module.scss';
 const Products = () => {
   // const { category, subcategory } = useParams();
   const products = useSelector(getProductsByQuery);
-  console.log(products);
+
   //   const [searchParams] = useSearchParams();
   //   const searchQuery = searchParams.get('search') ?? '';
   //   const dispatch = useDispatch();
 
-  useEffect(() => {
-    if (products.length < 1) {
-      return;
-    }
+  // useEffect(() => {
+  //   if (products.length < 1) {
+  //     return;
+  //   }
 
-    // dispatch(searchProducts(searchQuery));
-  }, [products.length]);
+  //   // dispatch(searchProducts(searchQuery));
+  // }, [products.length]);
 
   return (
     <section className={s.container}>
-      <ul className={s.listCard}>
-        {products.map(
-          ({
-            _id,
-            mainPhotoUrl,
-            price,
-            nameProduct,
-            description,
-            section,
-            category,
-          }) => (
-            <li className={s.itemCard} key={_id}>
-              <Link
-                to={`/products/${section}/${category}/${_id}`}
-                className={s.photoLink}
-              >
-                <div className={s.stylePhotoCardWrap}>
-                  <img
-                    className={s.photoCard}
-                    src={mainPhotoUrl}
-                    onError={e => (e.target.src = NoPhoto)}
-                    alt=""
-                  />
-                  <p className={s.descriptionProductCard}>{description}</p>
-                </div>
-              </Link>
+      {products.length > 0 && (
+        <ul className={s.listCard}>
+          {products.map(
+            ({
+              _id,
+              mainPhotoUrl,
+              price,
+              nameProduct,
+              description,
+              section,
+              category,
+            }) => (
+              <li className={s.itemCard} key={_id}>
+                <Link
+                  to={`/products/${section}/${category}/${_id}`}
+                  className={s.photoLink}
+                >
+                  <div className={s.stylePhotoCardWrap}>
+                    <img
+                      className={s.photoCard}
+                      src={mainPhotoUrl}
+                      onError={e => (e.target.src = NoPhoto)}
+                      alt=""
+                    />
+                    <p className={s.descriptionProductCard}>{description}</p>
+                  </div>
+                </Link>
 
-              <div className={s.stylePriceLike}>
-                <p className={s.priceCard}>{price}грн</p>
-                <div className={s.styleLike}>
-                  <p className={s.likeCard}>7</p>
-                  <NavLink to="/favorites" className={s.link}>
-                    <FiHeart size={24} />
-                  </NavLink>
+                <div className={s.stylePriceLike}>
+                  <p className={s.priceCard}>{price}грн</p>
+                  <div className={s.styleLike}>
+                    <p className={s.likeCard}>7</p>
+                    <NavLink to="/favorites" className={s.link}>
+                      <FiHeart size={24} />
+                    </NavLink>
+                  </div>
                 </div>
-              </div>
-              <Link to={`/products/${section}/${category}/${_id}`}>
-                <p className={s.nameProductCard}>{nameProduct}</p>
-              </Link>
-              <p className={s.sizeCard}>36</p>
-            </li>
-          )
-        )}
-      </ul>
+                <Link to={`/products/${section}/${category}/${_id}`}>
+                  <p className={s.nameProductCard}>{nameProduct}</p>
+                </Link>
+                <p className={s.sizeCard}>36</p>
+              </li>
+            )
+          )}
+        </ul>
+      )}
+      {products.length < 1 && <h1>За вашим запитом товарів не знайдено</h1>}
     </section>
   );
 };
