9060db6a0dbaa896d90cc151b8e4537fd5f09d94
Kateryna Kleshch <ketrins1993@gmail.com>
2023-06-20 16:51:20 +0300

    add photo collection

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,4 +1,4 @@
-import { useParams, useLocation, Link } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import Container from 'components/Shared/Container/Container';
 import s from './ProductCard.module.scss';
 import Text from 'components/Shared/Text/Text';
@@ -18,15 +18,14 @@ import PhotoCollection from 'components/Shared/PhotoCollection/PhotoCollection';
 const ProductCard = () => {
   const { category, subcategory, id } = useParams();
   const dispatch = useDispatch();
-console.log(category, subcategory, id);
+
   useEffect(() => {
     console.log("dispatch goooo");
     dispatch(getProductById(id));
   }, [dispatch, id]);
 
-  const location = useLocation();
   const product = useSelector(selectProductById);
-  console.log(location, id, product);
+  console.log( id, product);
 
   const {
     nameProduct,
@@ -38,105 +37,104 @@ console.log(category, subcategory, id);
     price,
     category: subSection,
   } = product;
-  // const backLinkHref = location.state?.from ?? '/products';
 
-  return (
-    <section className={s.productCard}>
-      <Container>
-        <p className={s.navigation}>
-          <Link to={`/`}>Easy shop </Link> &#8250;
-          <Link to={`/products/${category}`}> {category} </Link>&#8250;
-          <Link to={`/products/${category}/${subcategory}`}>
-            {' '}
-            {subcategory}
-          </Link>
-        </p>
-        <div className={s.productCardWrapper}>
-          <div>
-            <div className={s.productMainInfo}>
-              <div className={s.fotoContainer}>
-                <PhotoCollection
-                  mainPhotoUrl={mainPhotoUrl}
-                  nameProduct={nameProduct}
-                  additionalPhotoUrl={additionalPhotoUrl}
-                />
-                {/* <img
-                  src={mainPhotoUrl}
-                  alt={nameProduct}
-                  className={s.fotoGalery}
-                /> */}
+    return (
+      <section className={s.productCard}>
+        <Container>
+          <p className={s.navigation}>
+            <Link to={`/`}>Easy shop </Link> &#8250;
+            <Link to={`/products/${category}`}> {category} </Link>&#8250;
+            <Link to={`/products/${category}/${subcategory}`}>
+              {' '}
+              {subcategory}
+            </Link>
+          </p>
+          <div className={s.productCardWrapper}>
+            <div>
+              <div className={s.productMainInfo}>
+                <div className={s.fotoContainer}>
+                  {product !== {} && (
+                    <PhotoCollection
+                      mainPhotoUrl={mainPhotoUrl}
+                      nameProduct={nameProduct}
+                      additionalPhotoUrl={additionalPhotoUrl}
+                    />
+                  )}
+                </div>
+                <div className={s.productInfoWrapper}>
+                  <p className={s.availability}>В наявності</p>
+                  <Text text={nameProduct} textClass="productName" />
+                  <div className={s.productPrice}>
+                    <span className={s.productOldPrice}>379 грн</span>
+                    <span className={s.productPriceDiscount}>-8%</span>
+                    <Text text={price} textClass="title" />
+                  </div>
+                  <Text text="Розміри:" textClass="productLabels" />
+                  <div className={s.size}>
+                    <Text
+                      text={`EU: 40 / UA: 48 / IN: L `}
+                      textClass="after-title-bigger"
+                    />
+                  </div>
+                  <div className={s.buyBtns}>
+                    <Button
+                      type="button"
+                      btnClass="btnLight"
+                      text="Купити зараз"
+                    />
+                    <Button type="button" text="Додати до кошика" />
+                  </div>
+
+                  <div className={s.additionalOptsContainer}>
+                    <div className={s.additionalOpts}>
+                      <BsSuitHeart className={s.favoriteIcon} />
+                      <Text text="Додати в обрані" textClass="productText" />
+                    </div>
+                    <div className={s.additionalOpts}>
+                      <BiMessageDetail className={s.favoriteIcon} />
+                      <Text
+                        text="Поставити запитання"
+                        textClass="productText"
+                      />
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className={s.productInfoWrapper}>
-                <p className={s.availability}>В наявності</p>
-                <Text text={nameProduct} textClass="productName" />
-                <div className={s.productPrice}>
-                  <span className={s.productOldPrice}>379 грн</span>
-                  <span className={s.productPriceDiscount}>-8%</span>
-                  <Text text={price} textClass="title" />
-                </div>
-                <Text text="Розміри:" textClass="productLabels" />
-                <div className={s.size}>
-                  <Text
-                    text={`EU: 40 / UA: 48 / IN: L `}
-                    textClass="after-title-bigger"
-                  />
-                </div>
-                <div className={s.buyBtns}>
-                  <Button
-                    type="button"
-                    btnClass="btnLight"
-                    text="Купити зараз"
-                  />
-                  <Button type="button" text="Додати до кошика" />
-                </div>
 
-                <div className={s.additionalOptsContainer}>
-                  <div className={s.additionalOpts}>
-                    <BsSuitHeart className={s.favoriteIcon} />
-                    <Text text="Додати в обрані" textClass="productText" />
-                  </div>
-                  <div className={s.additionalOpts}>
-                    <BiMessageDetail className={s.favoriteIcon} />
-                    <Text text="Поставити запитання" textClass="productText" />
-                  </div>
+              <ul className={s.productInfo}>
+                <li className={s.productDescription}>
+                  <Text text="Стан:" textClass="productLabels" />
+                  <Text text={condition} textClass="productText" />
+                </li>
+                <li className={s.productDescription}>
+                  <Text text="Бренд:" textClass="productLabels" />
+                  <Text text={brendName} textClass="productText" />
+                </li>
+                <li className={s.productDescription}>
+                  <Text text="Категорії:" textClass="productLabels" />
+                  <Text text={subSection} textClass="productText" />
+                </li>
+              </ul>
+              <div className={s.productDetails}>
+                <div className={s.productDescription}>
+                  <Text text="Опис товару:" textClass="productLabels" />
+                  <Text text={description} textClass="productText" />
                 </div>
+                <DeliveryList />
               </div>
             </div>
-
-            <ul className={s.productInfo}>
-              <li className={s.productDescription}>
-                <Text text="Стан:" textClass="productLabels" />
-                <Text text={condition} textClass="productText" />
-              </li>
-              <li className={s.productDescription}>
-                <Text text="Бренд:" textClass="productLabels" />
-                <Text text={brendName} textClass="productText" />
-              </li>
-              <li className={s.productDescription}>
-                <Text text="Категорії:" textClass="productLabels" />
-                <Text text={subSection} textClass="productText" />
-              </li>
-            </ul>
-            <div className={s.productDetails}>
-              <div className={s.productDescription}>
-                <Text text="Опис товару:" textClass="productLabels" />
-                <Text text={description} textClass="productText" />
+            <div>
+              <Text text="Продавець:" textClass="productLabels" />
+              <div className={s.sellerInfo}>
+                <SellerInfo />
               </div>
-              <DeliveryList />
             </div>
           </div>
-          <div>
-            <Text text="Продавець:" textClass="productLabels" />
-            <div className={s.sellerInfo}>
-              <SellerInfo />
-            </div>
-          </div>
-        </div>
-        <Dialogue />
-        <textarea></textarea>
-      </Container>
-    </section>
-  );
+          <Dialogue />
+          <textarea></textarea>
+        </Container>
+      </section>
+    );
 };
 
 export default ProductCard;
