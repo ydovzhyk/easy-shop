ec299e6cf1c81047e402c0c442209961b0578fa5
Nataliia Hrynchuk <natagrinchukk@gmail.com>
2023-06-22 22:17:55 +0300

    Delete verifiEmail-selectors.js

--- src/redux/verifiEmail/verifiEmail-selectors.js
+++
@@ -1,3 +0,0 @@
-export const getVerifiEmail = ({ verifiEmail }) => verifiEmail.email;
-export const getVerifyMessage = ({ verifiEmail }) => verifiEmail.message;
-export const getLoadingVerifyEmail = ({ verifiEmail }) => verifiEmail.loading;
