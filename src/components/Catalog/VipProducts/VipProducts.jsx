91f3cea6fe70933cf6f83a8dd02eb57b7ec3fa68
Marina <99042644+MariOkner@users.noreply.github.com>
2023-06-28 17:01:06 +0300

    added pagination on VipProduct

--- src/components/Catalog/VipProducts/VipProducts.jsx
+++ src/components/Catalog/VipProducts/VipProducts.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useMediaQuery } from 'react-responsive';
 
 import ProductItem from '../../Shared/ProductItem/ProductItem';
+import Pagination from 'components/Shared/Pagination/Pagination';
 
 import { getVipProducts } from 'redux/product/product-operations';
 import {
@@ -17,6 +19,7 @@ const VipProducts = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const arrayVipProducts = useSelector(getVipProductCard);
   const vipPages = useSelector(getVipPages);
+  const isDesktop = useMediaQuery({ minWidth: 1280 });
 
   useEffect(() => {
     dispatch(getVipProducts(currentPage));
@@ -35,47 +38,74 @@ const VipProducts = () => {
     }
   };
 
+  // for pagination
+  const handlePageChange = page => {
+    setCurrentPage(page);
+  };
+
   return (
-    <div className={s.styleButtonList}>
-      {currentPage > 1 && (
-        <div
-          className={`${s.arrowButton} ${s.arrowButtonLeft}`}
-          onClick={handlePrevPage}
-        >
-          <FiChevronLeft
-            size={60}
-            strokeWidth={1}
-            className={s.arrowlinkLeft}
-          />
-        </div>
-      )}
-      <ul className={s.listCard}>
-        {arrayVipProducts.map(item => (
-          <ProductItem
-            key={item._id}
-            _id={item._id}
-            mainPhotoUrl={item.mainPhotoUrl}
-            price={item.price}
-            nameProduct={item.nameProduct}
-            description={item.description}
-            size={item.size}
-            section={item.section}
-            category={item.category}
-          />
-        ))}
-      </ul>
-      {currentPage < vipPages && (
-        <div
-          className={`${s.arrowButton} ${s.arrowButtonRight}`}
-          onClick={handleNextPage}
-        >
-          <FiChevronRight
-            size={60}
-            strokeWidth={1}
-            className={s.arrowlinkRigth}
-          />
-        </div>
-      )}
+    <div>
+      <div className={s.styleButtonList}>
+        {isDesktop && (
+          <>
+            {currentPage > 1 && (
+              <div
+                className={`${s.arrowButton} ${s.arrowButtonLeft}`}
+                onClick={handlePrevPage}
+              >
+                <FiChevronLeft
+                  size={60}
+                  strokeWidth={1}
+                  className={s.arrowlinkLeft}
+                />
+              </div>
+            )}
+          </>
+        )}
+        <ul className={s.listCard}>
+          {arrayVipProducts.map(item => (
+            <ProductItem
+              key={item._id}
+              _id={item._id}
+              mainPhotoUrl={item.mainPhotoUrl}
+              price={item.price}
+              nameProduct={item.nameProduct}
+              description={item.description}
+              size={item.size}
+              section={item.section}
+              category={item.category}
+            />
+          ))}
+        </ul>
+        {isDesktop && (
+          <>
+            {currentPage < vipPages && (
+              <div
+                className={`${s.arrowButton} ${s.arrowButtonRight}`}
+                onClick={handleNextPage}
+              >
+                <FiChevronRight
+                  size={60}
+                  strokeWidth={1}
+                  className={s.arrowlinkRigth}
+                />
+              </div>
+            )}
+          </>
+        )}
+      </div>
+
+      <div>
+        {!isDesktop && (
+          <div>
+            <Pagination
+              totalPages={vipPages}
+              currentPage={currentPage}
+              onPageChange={handlePageChange}
+            />
+          </div>
+        )}
+      </div>
     </div>
   );
 };
