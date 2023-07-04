789db9ad0c76d5d08ae51c1800538ca64749204b
Yuriy Dovzhyk <ydovzhyk@gmail.com>
2023-07-04 15:35:40 +0300

    add render to last visited page

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,17 +1,10 @@
 import React from 'react';
-// import { useLocation } from 'react-router-dom';
-// import {
-//   Suspense,
-//   useEffect,
-// } from 'react';
 import UserProfileInfo from 'components/Profile/UserProfileInfo/UserProfileInfo';
 import UserInfoDetails from './UserInfoDetails/UserInfoDetails';
 
 import s from './Profile.module.scss';
 
 const Profile = () => {
-  // const location = useLocation();
-
   return (
     <>
       <section className={s.profileavatar}>
