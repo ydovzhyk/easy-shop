b19fe8135ebbf631bf01445fc4f484b3a8cbe105
Yuriy Dovzhyk <ydovzhyk@gmail.com>
2023-07-06 14:44:35 +0300

    update

--- src/components/Dialogue/Dialogue.jsx
+++ src/components/Dialogue/Dialogue.jsx
@@ -1,4 +1,5 @@
 // import Container from 'components/Shared/Container/Container';
+import { getUserAvatar } from 'redux/auth/auth-selectors';
 import { BiMessageDetail } from 'react-icons/bi';
 import Text from 'components/Shared/Text/Text';
 import Avatar from 'components/Profile/Avatar/Avatar';
@@ -6,24 +7,26 @@ import Button from 'components/Shared/Button/Button';
 
 import {
   // useDispatch,
-  useSelector
+  useSelector,
 } from 'react-redux';
 
 import { getID } from 'redux/auth/auth-selectors';
 import { selectProductById } from 'redux/product/product-selectors';
-
 
 import s from './Dialogue.module.scss';
 
 const Dialogue = () => {
   const userId = useSelector(getID);
   const productById = useSelector(selectProductById);
+  const userAvatar = useSelector(getUserAvatar);
+
+  console.log('User avatar');
 
   const questionRequest = () => {
-    console.log("userId:", userId);
-    console.log("productById:", productById);
-    console.log("productOner:", productById.owner);
-    console.log("productId:", productById._id);
+    console.log('userId:', userId);
+    console.log('productById:', productById);
+    console.log('productOner:', productById.owner);
+    console.log('productId:', productById._id);
   };
 
   return (
@@ -33,7 +36,7 @@ const Dialogue = () => {
         <Text text="Поставити запитання" textClass="productText" />
       </div>
       <div className={s.avatar}>
-        <Avatar avatarClass="photoAvatar" />
+        <Avatar avatarClass="photoDialog" src={userAvatar} />
       </div>
       <textarea></textarea>
       <div className={s.questionButton}>
