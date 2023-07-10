196007b2f9a48be70d50a87bf824f3897043512b
Nataliia Hrynchuk <natagrinchukk@gmail.com>
2023-07-10 19:46:07 +0300

    Update Basket.jsx

--- src/components/Basket/Basket.jsx
+++ src/components/Basket/Basket.jsx
@@ -14,7 +14,7 @@ import Text from 'components/Shared/Text/Text';
 import BasketForm from 'components/Basket/BasketForm/BasketForm';
 // import RoundButton from 'components/Shared/RoundButton/RoundButton';
 // import CountBlock from './CountBlock/CountBlock';
-// import SizeSelection  from 'components/Basket/SizeSelection/SizeSelection';
+
 // import NoPhoto from 'images/catalog_photo/no_photo.jpg';
 
 
@@ -23,19 +23,17 @@ import s from './Basket.module.scss';
 
 const Basket = () => {
   const dispatch = useDispatch();
- 
+
   const isMessage = useSelector(getMessage);
   const user = useSelector(getUser);
   const productsFrombasket = useSelector(selectProductsFromBasket);
   const sellersFrombasket = useSelector(selectSellersFromBasket);
   const selectedProductsWithSizes = useSelector(selectBasketProducts);
-
-  
   const isTablet = useMediaQuery({ minWidth: 840 });
   
-  console.log('productsFrombasket:', productsFrombasket);
-  console.log('sellersfrombasket:', sellersFrombasket);
-  console.log('selectedProductsWithSizes:', selectedProductsWithSizes);
+  // console.log('productsFrombasket:', productsFrombasket);
+  // console.log('sellersfrombasket:', sellersFrombasket);
+  // console.log('selectedProductsWithSizes:', selectedProductsWithSizes);
   
   const groupedProducts = sellersFrombasket.map((seller) => {
   const ownerName = seller.username;
@@ -47,7 +45,7 @@ const Basket = () => {
         .flatMap((arr) => arr)
         .find((sp) => sp.productId === product._id);
       const selectedSizes = matchedProduct ? matchedProduct.selectedSizes : [];
-      console.log("matchedProduct:", matchedProduct)
+      // console.log("matchedProduct:", matchedProduct)
 
       return {
         _id: product._id,
@@ -67,32 +65,7 @@ const Basket = () => {
   };
 });
 
-
-  // const groupedProducts = sellersFrombasket.map((seller) => {
-  //   const ownerName = seller.username;
-  //   const ownerId = seller._id;
-  //   const products = productsFrombasket
-  //     .filter((product) => product.owner === ownerId)
-  //     .map((product) => ({
-  //       _id: product._id,
-  //       brendName: product.brendName,
-  //       nameProduct: product.nameProduct,
-  //       mainPhotoUrl: product.mainPhotoUrl,
-  //       price: product.price,
-  //       quantity: product.quantity,
-  //       size: product.size,
-
-  //     }));
-  //   return {
-  //     ownerId,
-  //     ownerName,
-  //     products
-  //   }
-  // })
-
-  console.log('groupedProducts:', groupedProducts);
-
-  
+  // console.log('groupedProducts:', groupedProducts);
 
   useEffect(() => {
     dispatch(getProductsFromBasket(user._id));
