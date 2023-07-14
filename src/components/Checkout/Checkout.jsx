242d6b3a19a996cf0dc5eab7674b63135c027e46
Kateryna Kleshch <ketrins1993@gmail.com>
2023-07-14 15:47:23 +0300

    started styles

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -15,13 +15,12 @@ import s from './Checkout.module.scss';
 const Checkout = () => {
   const productsInOrder = useSelector(selectProductsFromBasket);
   const user = useSelector(getUser);
-  console.log('user', user);
 
   const { secondName, firstName, surName, tel } = user || {};
 
   const sellerName = 'Katya';
   const orderSum = '750';
-  //control, register, handleSubmit, watch
+  
   const {
     control,
     handleSubmit,
@@ -42,7 +41,6 @@ const Checkout = () => {
 console.log('errors', errors);
   const onSubmit = (data, e) => {
     
-    console.log('data', data);
     e.preventDefault();
     const orderData = {
       sellerName: data.sellerName,
@@ -60,201 +58,204 @@ console.log('errors', errors);
     <Container>
       <section className={s.default}>
         <div className={s.defaultBox}>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <h2 className={s.title}>Оформлення замовлення</h2>
-            <Text
-              textClass="productHeadings"
-              text={`Продавець: ${sellerName}`}
-            />
-            <Text
-              textClass="title"
-              text={`Сума замовлення: ${orderSum} грн.`}
-            />
-            <ul>
-              {productsInOrder.map(
-                ({ _id, nameProduct, mainPhotoUrl, brendName, price }) => (
-                  <li className={s.productItem} key={_id}>
-                    <div className={s.infoWraper}>
-                      <div className={s.mainInfo}>
-                        <div className={s.thumb}>
-                          <img
-                            className={s.mainPhoto}
-                            src={mainPhotoUrl}
-                            onError={e => (e.target.src = NoPhoto)}
-                            alt={nameProduct}
-                          />
+          <form onSubmit={handleSubmit(onSubmit)} className={s.formWrapper}>
+            <div className={s.orderInfo}>
+              <h2 className={s.title}>Оформлення замовлення</h2>
+              <Text
+                textClass="productHeadings"
+                text={`Продавець: ${sellerName}`}
+              />
+              <Text
+                textClass="title"
+                text={`Сума замовлення: ${orderSum} грн.`}
+              />
+              <ul>
+                {productsInOrder.map(
+                  ({ _id, nameProduct, mainPhotoUrl, brendName, price }) => (
+                    <li className={s.productItem} key={_id}>
+                      <div className={s.infoWraper}>
+                        <div className={s.mainInfo}>
+                          <div className={s.thumb}>
+                            <img
+                              className={s.mainPhoto}
+                              src={mainPhotoUrl}
+                              onError={e => (e.target.src = NoPhoto)}
+                              alt={nameProduct}
+                            />
+                          </div>
+                          <div className={s.description}>
+                            <Text textClass="productText" text={brendName} />
+                            <Text
+                              textClass="productHeadings"
+                              text={nameProduct}
+                            />
+                            <Text textClass="productText" text="Size" />
+                          </div>
                         </div>
-                        <div className={s.description}>
-                          <Text textClass="productText" text={brendName} />
+                        <div className={s.priceWrapper}>
+                          <Text textClass="productText" text="Кількість: 1" />
                           <Text
                             textClass="productHeadings"
-                            text={nameProduct}
+                            text={`Сума: ${price}`}
                           />
-                          <Text textClass="productText" text="Size" />
                         </div>
                       </div>
-                      <div className={s.priceWrapper}>
-                        <Text textClass="productText" text="Кількість: 1" />
-                        <Text
-                          textClass="productHeadings"
-                          text={`Сума: ${price}`}
-                        />
-                      </div>
-                    </div>
-                  </li>
-                )
-              )}
-            </ul>
-            <div className={s.formField}>
-              <Text textClass="title" text="Спосіб доставки" />
-              <Controller
-                control={control}
-                name="delivery"
-                rules={{
-                  required: true,
-                }}
-                render={({ field: { onChange, value } }) => (
-                  <SelectField
-                    value={value}
-                    handleChange={onChange}
-                    className="addOrder"
-                    {...field.delivery}
-                    options={['Нова пошта', 'УкрПошта', 'Meest']}
-                    {...register('delivery', {
-                      required: 'Виберіть службу доставки',
-                    })}
-                  />
+                    </li>
+                  )
                 )}
-              />
-              {errors.delivery && (
-                <p className={s.inputError}>{errors.delivery.message}</p>
-              )}
-            </div>
-            <div>
-              <Text textClass="title" text="Ваші контактні дані" />
+              </ul>
               <div className={s.formField}>
-                <Text text={'Прізвище*'} textClass="productHeadings" />
+                <Text textClass="title" text="Спосіб доставки" />
                 <Controller
                   control={control}
-                  name="secondName"
+                  name="delivery"
                   rules={{
                     required: true,
                   }}
                   render={({ field: { onChange, value } }) => (
-                    <TextField
-                      className="addOrder"
+                    <SelectField
                       value={value}
-                      control={control}
                       handleChange={onChange}
-                      {...field.secondName}
-                      required={true}
-                      {...register('secondName', {
-                        required: "Обов'язково до заповнення",
+                      className="addOrder"
+                      {...field.delivery}
+                      options={['Нова пошта', 'УкрПошта', 'Meest']}
+                      {...register('delivery', {
+                        required: 'Виберіть службу доставки',
                       })}
                     />
                   )}
                 />
-                {errors.secondName && (
-                  <p className={s.inputError}>{errors.secondName.message}</p>
+                {errors.delivery && (
+                  <p className={s.inputError}>{errors.delivery.message}</p>
                 )}
               </div>
-              <div className={s.formField}>
-                <Text text={"Ім'я*"} textClass="productHeadings" />
-                <Controller
-                  control={control}
-                  name="firstName"
-                  rules={{
-                    required: true,
-                  }}
-                  render={({ field: { onChange, value } }) => (
-                    <TextField
-                      className="addOrder"
-                      value={value}
-                      control={control}
-                      handleChange={onChange}
-                      {...field.firstName}
-                      required={true}
-                      {...register('firstName', {
-                        required: "Обов'язково до заповнення",
-                      })}
-                    />
-                  )}
-                />
-                {errors.firstName && (
-                  <p className={s.inputError}>{errors.firstName.message}</p>
-                )}
-              </div>
-
-              <div className={s.formField}>
-                <Text text={'По батькові*'} textClass="productHeadings" />
-                <Controller
-                  control={control}
-                  name="surName"
-                  rules={{
-                    required: true,
-                  }}
-                  render={({ field: { onChange, value } }) => (
-                    <TextField
-                      className="addOrder"
-                      value={value}
-                      control={control}
-                      handleChange={onChange}
-                      {...field.surName}
-                      required={true}
-                      {...register('surName', {
-                        required: "Обов'язково до заповнення",
-                      })}
-                    />
-                  )}
-                />
-                {errors.surName && (
-                  <p className={s.inputError}>{errors.surName.message}</p>
-                )}
-              </div>
-
-              <div className={s.formField}>
-                <Text
-                  text={'Телефон +380*'}
-                  textClass="productHeadings"
-                  type="tel"
-                />
-                <Controller
-                  control={control}
-                  name="tel"
-                  rules={{
-                    required: true,
-                  }}
-                  render={({ field: { onChange, value } }) => (
-                    <TextField
-                      className="addOrder"
-                      value={value}
-                      control={control}
-                      handleChange={onChange}
-                      {...field.tel}
-                      required={true}
-                      {...register('tel', {
-                        required: "Обов'язково до заповнення",
-                        pattern: {
-                          value: /^\+?3?8?[0-9]{10}/,
-                          message: 'Приклад номеру : +380993453451',
-                        },
-                      })}
-                    />
-                  )}
-                />
-                {errors.tel && (
-                  <p className={s.inputError}>{errors.tel.message}</p>
-                )}
-              </div>
-            </div>
-            <div>
-              <Text textClass="title" text="Разом" />
               <div>
-                <span>Вартість товару</span>
+                <Text textClass="title" text="Ваші контактні дані" />
+                <div className={s.formField}>
+                  <Text text={'Прізвище*'} textClass="productHeadings" />
+                  <Controller
+                    control={control}
+                    name="secondName"
+                    rules={{
+                      required: true,
+                    }}
+                    render={({ field: { onChange, value } }) => (
+                      <TextField
+                        className="addOrder"
+                        value={value}
+                        control={control}
+                        handleChange={onChange}
+                        {...field.secondName}
+                        required={true}
+                        {...register('secondName', {
+                          required: "Обов'язково до заповнення",
+                        })}
+                      />
+                    )}
+                  />
+                  {errors.secondName && (
+                    <p className={s.inputError}>{errors.secondName.message}</p>
+                  )}
+                </div>
+                <div className={s.formField}>
+                  <Text text={"Ім'я*"} textClass="productHeadings" />
+                  <Controller
+                    control={control}
+                    name="firstName"
+                    rules={{
+                      required: true,
+                    }}
+                    render={({ field: { onChange, value } }) => (
+                      <TextField
+                        className="addOrder"
+                        value={value}
+                        control={control}
+                        handleChange={onChange}
+                        {...field.firstName}
+                        required={true}
+                        {...register('firstName', {
+                          required: "Обов'язково до заповнення",
+                        })}
+                      />
+                    )}
+                  />
+                  {errors.firstName && (
+                    <p className={s.inputError}>{errors.firstName.message}</p>
+                  )}
+                </div>
+
+                <div className={s.formField}>
+                  <Text text={'По батькові*'} textClass="productHeadings" />
+                  <Controller
+                    control={control}
+                    name="surName"
+                    rules={{
+                      required: true,
+                    }}
+                    render={({ field: { onChange, value } }) => (
+                      <TextField
+                        className="addOrder"
+                        value={value}
+                        control={control}
+                        handleChange={onChange}
+                        {...field.surName}
+                        required={true}
+                        {...register('surName', {
+                          required: "Обов'язково до заповнення",
+                        })}
+                      />
+                    )}
+                  />
+                  {errors.surName && (
+                    <p className={s.inputError}>{errors.surName.message}</p>
+                  )}
+                </div>
+
+                <div className={s.formField}>
+                  <Text
+                    text={'Телефон +380*'}
+                    textClass="productHeadings"
+                    type="tel"
+                  />
+                  <Controller
+                    control={control}
+                    name="tel"
+                    rules={{
+                      required: true,
+                    }}
+                    render={({ field: { onChange, value } }) => (
+                      <TextField
+                        className="addOrder"
+                        value={value}
+                        control={control}
+                        handleChange={onChange}
+                        {...field.tel}
+                        required={true}
+                        {...register('tel', {
+                          required: "Обов'язково до заповнення",
+                          pattern: {
+                            value: /^\+?3?8?[0-9]{10}/,
+                            message: 'Приклад номеру : +380993453451',
+                          },
+                        })}
+                      />
+                    )}
+                  />
+                  {errors.tel && (
+                    <p className={s.inputError}>{errors.tel.message}</p>
+                  )}
+                </div>
+              </div>
+            </div>
+            <div className={s.sumWrapper}>
+              <p className={s.sumTitle}>Разом</p>
+
+              <div className={s.sumBox}>
+                <Text textClass="title" text="Вартість товару" />
                 <span>{orderSum}</span>
               </div>
-              <div>
-                <span>До оплати</span>
+              <div className={s.sumBox}>
+                <Text textClass="title" text="До оплати" />
                 <span>{orderSum}</span>
               </div>
               <Button
