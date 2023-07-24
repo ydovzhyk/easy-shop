ed2fbd23a477aa2fab4c165fc2e2a03e7161123d
Kateryna Kleshch <ketrins1993@gmail.com>
2023-07-24 21:37:56 +0300

    started MyPurchases

--- src/components/Profile/MyPurchases/MyPurchases.jsx
+++ src/components/Profile/MyPurchases/MyPurchases.jsx
@@ -1,7 +1,11 @@
+import Container from "components/Shared/Container/Container";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getUserOrders } from "redux/order/order-operations";
 import { selectUserOrders } from "redux/order/order-selectors";
+
+import s from './MyPurchases.module.scss';
+import Text from "components/Shared/Text/Text";
 
 const MyShoppings = () => {
     const dispatch = useDispatch();
@@ -12,8 +16,36 @@ const MyShoppings = () => {
     }, [dispatch, page]);
     userOrders && console.log(userOrders);
     return (
+      <Container>
         <h3>Мої покупки</h3>
-    )
+        <div className={s.ordersWrapper}>
+          <div>
+            <Text textClass="title" text={`За статусом`} />
+            <ul className={s.optionsList}>
+              <li>Нові</li>
+              <li>Підтверджені</li>
+              <li>Виконані</li>
+              <li>Відхилені</li>
+              <li>Архівні</li>
+            </ul>
+          </div>
+          <ul>
+            {/* {userOrders !== [] && userOrders.orders.map(
+              ({ _id, sellerName, orderSum, orderDate, orderNumber }) => (
+                <li className={s.orderItem} key={_id}>
+                  <div className={s.box}>
+                    <p className={s.boxItem}>{sellerName}</p>
+                    <p className={s.boxItem}>{`Сума: ${orderSum}`}</p>
+                    <p className={s.boxItem}>{`Дата: ${orderDate}`}</p>
+                    <p className={s.boxItem}>{`Номер: ${orderNumber}`}</p>
+                  </div>
+                </li>
+              )
+            )} */}
+          </ul>
+        </div>
+      </Container>
+    );
 }
 
 export default MyShoppings;
