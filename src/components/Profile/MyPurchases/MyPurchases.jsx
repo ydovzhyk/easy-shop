db8355e2593ce38a2fb08ca2d0e5868bc2f7e8fe
Kateryna Kleshch <ketrins1993@gmail.com>
2023-07-26 15:13:31 +0300

    add some styles of purchases

--- src/components/Profile/MyPurchases/MyPurchases.jsx
+++ src/components/Profile/MyPurchases/MyPurchases.jsx
@@ -1,14 +1,14 @@
-import Container from "components/Shared/Container/Container";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { deleteOrderById, getUserOrders } from "redux/order/order-operations";
+// import { deleteOrderById } from "redux/order/order-operations";
+import { getUserOrders } from 'redux/order/order-operations';
 import {
   selectUserOrders,
   selectUserOrdersTotalPages,
 } from 'redux/order/order-selectors';
 
 import s from './MyPurchases.module.scss';
-import Text from "components/Shared/Text/Text";
+import OrderProductsList from "components/Shared/OrderProductsList/OrderProductsList";
 
 const MyShoppings = () => {
     const dispatch = useDispatch();
@@ -24,45 +24,58 @@ const MyShoppings = () => {
     const totalPages = useSelector(selectUserOrdersTotalPages);
     totalPages && console.log(totalPages);
 
-    const handleDeteleOrder = (id) => {
-      dispatch(deleteOrderById(id));
-    }
+    // const handleDeteleOrder = (id) => {
+    //   dispatch(deleteOrderById(id));
+    // }
     return (
-      <Container>
-        <h3>Мої покупки</h3>
+      <>
         <div className={s.ordersWrapper}>
           <div>
-            <Text textClass="title" text={`За статусом`} />
+            <p className={s.heading}>За статусом</p>
             <ul className={s.optionsList}>
               <li>Нові</li>
               <li>Підтверджені</li>
               <li>Виконані</li>
               <li>Відхилені</li>
-              <li>Архівні</li>
             </ul>
           </div>
           <ul className={s.ordersList}>
             {userOrders?.map(
-              ({ _id, sellerName, orderSum, orderDate, orderNumber }) => (
+              ({
+                _id,
+                sellerName,
+                orderSum,
+                orderDate,
+                orderNumber,
+                products,
+                productInfo,
+              }) => (
                 <li className={s.orderItem} key={_id}>
-                  <div className={s.box}>
-                    <p className={s.boxItem}>{sellerName}</p>
-                    <p className={s.boxItem}>{`Сума: ${orderSum}`}</p>
-                    <p className={s.boxItem}>{`Дата: ${orderDate}`}</p>
-                    <p className={s.boxItem}>{`Номер: ${orderNumber}`}</p>
-                    <button
+                  <div className={s.orderInfoWrapper}>
+                    <div className={s.orderInfoItem}>
+                      <p>Продавець:</p>
+                      <p>{sellerName}</p>
+                    </div>
+                    <div className={s.orderInfoItem}>
+                      <p>Замовлення &#8470; {orderNumber}</p>
+                      <p>{orderDate}</p>
+                    </div>
+
+                    {/* <button
                       type="button"
                       onClick={() => handleDeteleOrder(_id)}
                     >
-                      delete
-                    </button>
+                      del
+                    </button> */}
                   </div>
+                  <OrderProductsList productsForOrder={productInfo} products={products}/>
+                  <p className={s.orderSum}>{`Сума: ${orderSum}`}</p>
                 </li>
               )
             )}
           </ul>
         </div>
-      </Container>
+      </>
     );
 }
 
