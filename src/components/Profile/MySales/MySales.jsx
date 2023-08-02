47c376e56bd9ab4a1492259ef08b240f3820cc4a
Kateryna Kleshch <ketrins1993@gmail.com>
2023-08-02 21:26:32 +0300

    add Mysales component

---
+++ src/components/Profile/MySales/MySales.jsx
@@ -0,0 +1,178 @@
+import { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+// import { deleteOrderById } from "redux/order/order-operations";
+import {  getUserSales } from 'redux/order/order-operations';
+import {
+  getLoadingOrders,
+  selectUserSales,
+  selectUserSalesTotalPages,
+} from 'redux/order/order-selectors';
+import { getLogin } from 'redux/auth/auth-selectors';
+
+import OrderProductsList from 'components/Shared/OrderProductsList/OrderProductsList';
+import Pagination from 'components/Shared/Pagination/Pagination';
+import s from './MySales.module.scss';
+
+const MySales = () => {
+    const dispatch = useDispatch();
+    const isLogin = useSelector(getLogin);
+    const isLoading = useSelector(getLoadingOrders);
+    const userSales = useSelector(selectUserSales);
+    const totalPages = useSelector(selectUserSalesTotalPages);
+
+    const [currentPage, setCurrentPage] = useState(1);
+    const [currentSelector, setcurrentSelector] = useState('all');
+    console.log(currentSelector);
+
+  useEffect(() => {
+    dispatch(
+      getUserSales({
+        page: currentPage,
+        // selectorName: currentSelector,
+      })
+    );
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }, [dispatch, currentPage]);
+
+
+
+  // for pagination
+  const handlePageChange = page => {
+    setCurrentPage(page);
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  const handleButtonClick = optionName => {
+    setcurrentSelector(optionName);
+    setCurrentPage(1);
+  };
+  // const handleDeteleOrder = (id) => {
+  //   dispatch(deleteOrderById(id));
+  // }
+  return (
+    <>
+      <div className={s.ordersWrapper}>
+        <div>
+          <p className={s.heading}>За статусом</p>
+          <ul className={s.optionsList}>
+            <li>
+              <button
+                className={
+                  currentSelector === 'all'
+                    ? `${s.selectButton} ${s.active}`
+                    : s.selectButton
+                }
+                onClick={() => handleButtonClick('all')}
+              >
+                Всі
+              </button>
+            </li>
+            <li>
+              <button
+                className={
+                  currentSelector === 'new'
+                    ? `${s.selectButton} ${s.active}`
+                    : s.selectButton
+                }
+                onClick={() => handleButtonClick('new')}
+              >
+                Нові
+              </button>
+            </li>
+            <li>
+              <button
+                className={
+                  currentSelector === 'confirmed'
+                    ? `${s.selectButton} ${s.active}`
+                    : s.selectButton
+                }
+                onClick={() => handleButtonClick('confirmed')}
+              >
+                Підтверджені
+              </button>
+            </li>
+            {/* <li>Виконані</li> */}
+            <li>
+              <button
+                className={
+                  currentSelector === 'canceled'
+                    ? `${s.selectButton} ${s.active}`
+                    : s.selectButton
+                }
+                onClick={() => handleButtonClick('canceled')}
+              >
+                Відхилені
+              </button>
+            </li>
+          </ul>
+        </div>
+        {userSales.length > 0 && (
+          <ul className={s.ordersList}>
+            {userSales.map(
+              ({
+                _id,
+                orderSum,
+                orderDate,
+                orderNumber,
+                products,
+                productInfo,
+                client,
+              }) => (
+                <li className={s.orderItem} key={_id}>
+                  <div className={s.orderInfoWrapper}>
+                    <div className={s.orderInfoItem}>
+                      <p>Покупець:</p>
+                      <p>
+                        {client.customerSecondName} {client.customerFirstName}
+                      </p>
+                      <p>Номер телефону: {client.customerTel}</p>
+                    </div>
+                    <div className={s.orderInfoItem}>
+                      <p>Замовлення &#8470; {orderNumber}</p>
+                      <p>{orderDate}</p>
+                    </div>
+                    {/* <button
+                      type="button"
+                      onClick={() => handleDeteleOrder(_id)}
+                    >
+                      del
+                    </button> */}
+                  </div>
+                  <OrderProductsList
+                    productsForOrder={productInfo}
+                    products={products}
+                  />
+                  <div className={s.orderBottomWrapper}>
+                    <NavLink
+                      to={isLogin ? '/checkout' : '/login'}
+                      className={s.btnLight}
+                    >
+                      Підтвердити замовлення
+                    </NavLink>
+
+                    <p
+                      className={s.orderSum}
+                    >{`Сума замовлення: ${orderSum} грн.`}</p>
+                  </div>
+                </li>
+              )
+            )}
+          </ul>
+        )}
+        {!isLoading && userSales.length === 0 && (
+          <p className={s.message}>Замовлень з таким статусом у вас ще немає</p>
+        )}
+      </div>
+      {totalPages > 1 && (
+        <Pagination
+          totalPages={totalPages}
+          currentPage={currentPage}
+          onPageChange={handlePageChange}
+        />
+      )}
+    </>
+  );
+};
+
+export default MySales;
