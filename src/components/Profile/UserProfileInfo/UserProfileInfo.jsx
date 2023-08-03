0d5f3e01335c149505c634d9b0be3467e47bbd5a
Yuriy Dovzhyk <ydovzhyk@gmail.com>
2023-08-03 14:59:16 +0300

    updated Exit

--- src/components/Profile/UserProfileInfo/UserProfileInfo.jsx
+++ src/components/Profile/UserProfileInfo/UserProfileInfo.jsx
@@ -1,23 +1,42 @@
-import { useSelector } from 'react-redux';
-import { BsCheck2, BsGeoAlt, BsHandbag, BsPeople } from 'react-icons/bs';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive';
 import {
   getUserName,
   getUser,
   getUserAvatar,
   getUserDateCreate,
 } from 'redux/auth/auth-selectors';
+import {
+  clearDialoguesArray,
+  clearDialogue,
+} from 'redux/dialogue/dialogue-slice';
+import { clearOtherUser } from 'redux/otherUser/otherUser.slice';
+import { logout } from 'redux/auth/auth-opetations';
 import Container from 'components/Shared/Container';
 import Avatar from 'components/Profile/Avatar/Avatar';
 import UserRating from 'components/Profile/UserProfileInfo/UserRating';
 import Value from 'components/Profile/Value';
 import DaysValue from 'components/Shared/helper/DaysValue';
+import Button from 'components/Shared/Button';
+import {
+  BsCheck2,
+  BsGeoAlt,
+  BsHandbag,
+  BsPeople,
+  BsEscape,
+} from 'react-icons/bs';
+
 import s from 'components/Profile/UserProfileInfo/UserProfileInfo.module.scss';
 
 const UserProfileInfo = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const userName = useSelector(getUserName);
   const user = useSelector(getUser);
   const userAvatar = useSelector(getUserAvatar);
   const dateCreate = useSelector(getUserDateCreate);
+  const isTablet = useMediaQuery({ minWidth: 768 });
 
   const getDaysPassedFromDate = dateString => {
     const date = new Date(dateString);
@@ -33,6 +52,21 @@ const UserProfileInfo = () => {
   const daysAmount = getDaysPassedFromDate(dateCreate);
   const followersAmount = 36;
   const salesAmount = 16;
+
+  const onLogout = async () => {
+    navigate('/');
+    await dispatch(logout());
+    const authData = {
+      accessToken: null,
+      refreshToken: null,
+      sid: null,
+    };
+    dispatch(clearDialogue());
+    dispatch(clearDialoguesArray());
+    dispatch(clearOtherUser());
+    await localStorage.setItem('easy-shop.authData', JSON.stringify(authData));
+  };
+
   return (
     <Container>
       <div className={s.profilewrapper}>
@@ -74,6 +108,22 @@ const UserProfileInfo = () => {
               </p>
             </div>
           </div>
+        </div>
+        <div className={s.wrapper}>
+          <BsEscape
+            size={25}
+            style={{
+              marginRight: isTablet ? '10px' : '0px',
+              marginBottom: isTablet ? '0px' : '10px',
+              color: 'var(--icons-color)',
+            }}
+          />
+          <Button
+            text="Вихід"
+            type="button"
+            handleClick={onLogout}
+            btnClass="exitHeaderBtn"
+          />
         </div>
       </div>
     </Container>
