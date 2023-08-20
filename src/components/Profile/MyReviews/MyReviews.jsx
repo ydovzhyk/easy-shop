7b97d3020fd9aa341547682926560e17874a2b26
Kateryna Kleshch <ketrins1993@gmail.com>
2023-08-20 18:53:16 +0300

    add review list

--- src/components/Profile/MyReviews/MyReviews.jsx
+++ src/components/Profile/MyReviews/MyReviews.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import s from './MyReviews.module.scss';
+
 import { selectUserFeedback, selectUserReviews } from 'redux/review/review-selectors';
 import { getUserFeedback, getUserReviews } from 'redux/review/review-operations';
 import { getID } from 'redux/auth/auth-selectors';
 import { clearReviewAndFeedback } from 'redux/review/review-slice';
+import Avatar from 'components/Profile/Avatar/Avatar';
+import StarsList from 'components/Shared/StarsList/StarsList';
+import s from './MyReviews.module.scss';
 
 const MyReviews = () => {
     const dispatch = useDispatch();
@@ -25,6 +28,7 @@ const MyReviews = () => {
   const handleButtonClick = optionName => {
     setcurrentSelector(optionName);
   };
+  const review = currentSelector === 'seller' ? myFeedback : myReview;
   return (
     <div className={s.reviewsPageWrapper}>
       <div className={s.reviewsListWrapper}>
@@ -56,6 +60,37 @@ const MyReviews = () => {
           </li>
         </ul>
       </div>
+      {review.length === 0 && (
+        <p className={s.message}>Тут поки нічого немає</p>
+      )}
+      <ul className={s.reviewsWrapper}>
+        {review.map(
+          ({ _id, reviewer, rating, reviewDate, products, feedback }) => {
+            return (
+              <li className={s.reviewBox} key={_id}>
+                <div className={s.avatarBox}>
+                  <Avatar src={reviewer.reviewerFoto} />
+                </div>
+                <div className={s.reviewWrapper}>
+                  <div className={s.topReviewWrapper}>
+                    <div>
+                      <p className={s.reviewerName}>{reviewer.reviewerName}</p>
+                      <StarsList rating={rating} size={16} />
+                    </div>
+                    <p>{reviewDate}</p>
+                  </div>
+                  <ul className={s.productName}>
+                    {products.map(product => (
+                      <li key={product._id}>{product.nameProduct}</li>
+                    ))}
+                  </ul>
+                  <p>{feedback}</p>
+                </div>
+              </li>
+            );
+          }
+        )}
+      </ul>
     </div>
   );
 };
