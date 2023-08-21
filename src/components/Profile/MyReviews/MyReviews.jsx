b9701459fb32056850d1a3cf79ff578d60945fe8
Kateryna Kleshch <ketrins1993@gmail.com>
2023-08-22 01:23:51 +0300

    fix mobile version

--- src/components/Profile/MyReviews/MyReviews.jsx
+++ src/components/Profile/MyReviews/MyReviews.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useMediaQuery } from 'react-responsive';
 
 import { getLoadingReviews, selectUserFeedback, selectUserReviews } from 'redux/review/review-selectors';
 import { getUserFeedback, getUserReviews } from 'redux/review/review-operations';
 import { getID } from 'redux/auth/auth-selectors';
 import { clearReviewAndFeedback } from 'redux/review/review-slice';
+
+import Select from 'react-select';
 import Avatar from 'components/Profile/Avatar/Avatar';
 import StarsList from 'components/Shared/StarsList/StarsList';
 import s from './MyReviews.module.scss';
@@ -15,6 +18,7 @@ const MyReviews = () => {
     const myReview = useSelector(selectUserReviews);
     const myFeedback = useSelector(selectUserFeedback);
     const loading = useSelector(getLoadingReviews);
+    const isTablet = useMediaQuery({ minWidth: 768 });
     
     const [currentSelector, setcurrentSelector] = useState('seller');
     useEffect(() => {
@@ -34,7 +38,22 @@ const MyReviews = () => {
     <div className={s.reviewsPageWrapper}>
       <div className={s.reviewsListWrapper}>
         <p className={s.heading}>Відгуки</p>
-        <ul className={s.optionsList}>
+        {!isTablet && (
+          <Select
+            classNamePrefix="custom-select"
+            onChange={value => handleButtonClick(value.value)}
+            options={[
+              { value: 'seller', label: 'Як продавця' },
+              { value: 'client', label: 'Як покупця' },
+            ]}
+            defaultValue={{ value: 'seller', label: 'Як продавця' }}
+            theme={theme => ({
+              ...theme,
+              borderRadius: 0,
+            })}
+          />
+        )}
+        {isTablet && <ul className={s.optionsList}>
           <li>
             <button
               className={
@@ -59,7 +78,7 @@ const MyReviews = () => {
               Як покупця
             </button>
           </li>
-        </ul>
+        </ul>}
       </div>
       {!loading && review.length === 0 && (
         <p className={s.message}>Тут поки нічого немає</p>
