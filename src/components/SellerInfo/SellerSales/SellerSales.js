b3146f815c9973fd453c91668d38dd55a7846c4e
Nataliia Hrynchuk <natagrinchukk@gmail.com>
2023-08-25 17:39:41 +0300

    Create SellerSales.js

---
+++ src/components/SellerInfo/SellerSales/SellerSales.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import OrderStatusList from 'components/Shared/OrderStatusList/OrderStatusList';
+
+import s from 'components/SellerInfo/SellerSales/SellerSales.module.scss';
+
+const SellerSales = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [currentSelector, setcurrentSelector] = useState('all');
+
+  const handleButtonClick = optionName => {
+    setcurrentSelector(optionName);
+    setCurrentPage(1);
+  };
+
+  return (
+    <>
+      <div className={s.ordersWrapper}>
+        <OrderStatusList
+          currentSelector={currentSelector}
+          handleButtonClick={handleButtonClick}
+        />
+      </div>
+    </>
+  );
+};
+
+export default SellerSales;
