b02b84867a62717058816e5946e9c0644288c9ba
Yuriy Dovzhyk <48929257+ydovzhyk@users.noreply.github.com>
2023-08-25 18:08:36 +0300

    Update SellerSales.js

--- src/components/SellerInfo/SellerSales/SellerSales.js
+++ src/components/SellerInfo/SellerSales/SellerSales.js
@@ -1,15 +1,15 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import OrderStatusList from 'components/Shared/OrderStatusList/OrderStatusList';
 
 import s from 'components/SellerInfo/SellerSales/SellerSales.module.scss';
 
 const SellerSales = () => {
-  const [currentPage, setCurrentPage] = useState(1);
+  // const [currentPage, setCurrentPage] = useState(1);
   const [currentSelector, setcurrentSelector] = useState('all');
 
   const handleButtonClick = optionName => {
     setcurrentSelector(optionName);
-    setCurrentPage(1);
+    // setCurrentPage(1);
   };
 
   return (
