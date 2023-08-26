2220a3937f4d31972c386f1c3452b794ca622e17
Yuliya Zozulya <djulia@ukr.net>
2023-08-27 00:38:33 +0300

    fix

--- src/components/Developers/Developers.jsx
+++ src/components/Developers/Developers.jsx
@@ -1,39 +1,9 @@
-import Container from 'components/Shared/Container';
-import s from './Developers.module.scss';
-// import Avatar from 'components/Profile/Avatar/Avatar';
+import { BsGithub, BsLinkedin } from 'react-icons/bs';
 
-const developers = [
-  {
-    devName: 'Юрій Довжик',
-    devPhoto: 'https://avatars.githubusercontent.com/u/48929257?v=4',
-    gitHubLink: 'https://github.com/ydovzhyk',
-  },
-  {
-    devName: 'Наталія Гринук',
-    devPhoto: 'https://avatars.githubusercontent.com/u/92783918?v=4',
-    gitHubLink: 'https://github.com/NataliiaHrynchuk',
-  },
-  {
-    devName: 'Катерина Клещ',
-    devPhoto: 'https://avatars.githubusercontent.com/u/99125819?v=4',
-    gitHubLink: 'https://github.com/kotyamur',
-  },
-  {
-    devName: 'Юлія Зозуля',
-    devPhoto: 'https://avatars.githubusercontent.com/u/98358870?v=4',
-    gitHubLink: 'https://github.com/Immayou',
-  },
-  {
-    devName: 'Марина Петренко',
-    devPhoto: 'https://avatars.githubusercontent.com/u/99042644?v=4',
-    gitHubLink: 'https://github.com/MariOkner',
-  },
-  {
-    devName: 'Юлія Дольнікова',
-    devPhoto: 'https://avatars.githubusercontent.com/u/98531699?v=4',
-    gitHubLink: 'https://github.com/Dolnikova',
-  },
-];
+import Container from 'components/Shared/Container';
+import developers from './developersList';
+import s from './Developers.module.scss';
+import Text from 'components/Shared/Text/Text';
 
 const Developers = () => {
   return (
@@ -41,14 +11,22 @@ const Developers = () => {
       <Container>
         <h1 className={s.developersHeading}>Розробники сайту</h1>
         <ul className={s.developersList}>
-          {developers.map(({ devName, devPhoto, gitHubLink }) => (
+          {developers.map(({ devName, devPhoto, gitHubLink, linkedInLink }) => (
             <li className={s.listItem} key={devName}>
               <div className={s.avatarWrapper}>
-                {/* <Avatar src={devPhoto} /> */}
                 <img src={devPhoto} alt={`developer ${devName}`} />
               </div>
-              <p>{devName}</p>
-              <a href={gitHubLink}>Посилання на GitHub</a>
+              <div className={s.bottomContentWrapper}>
+                <Text text={devName} textClass="cardMainText" />
+                <div className={s.bottomSocialContentWrapper}>
+                  <a href={gitHubLink} className={s.socialLink}>
+                    <BsGithub size={35} className={s.socialIcon} />
+                  </a>
+                  <a href={linkedInLink}>
+                    <BsLinkedin size={35} className={s.socialIcon} />
+                  </a>
+                </div>
+              </div>
             </li>
           ))}
         </ul>
---
+++ src/components/Developers/developersList.js
@@ -0,0 +1,40 @@
+const developers = [
+  {
+    devName: 'Юрій Довжик',
+    devPhoto: 'https://avatars.githubusercontent.com/u/48929257?v=4',
+    gitHubLink: 'https://github.com/ydovzhyk',
+    linkedInLink: 'https://www.linkedin.com/in/yuriy-dovzhyk/',
+  },
+  {
+    devName: 'Наталія Гринук',
+    devPhoto: 'https://avatars.githubusercontent.com/u/92783918?v=4',
+    gitHubLink: 'https://github.com/NataliiaHrynchuk',
+    linkedInLink: 'https://www.linkedin.com/in/nataliia-hrynchuk/',
+  },
+  {
+    devName: 'Катерина Клещ',
+    devPhoto: 'https://avatars.githubusercontent.com/u/99125819?v=4',
+    gitHubLink: 'https://github.com/kotyamur',
+    linkedInLink: 'https://www.linkedin.com/in/kateryna-kleshch/',
+  },
+  {
+    devName: 'Юлія Зозуля',
+    devPhoto: 'https://avatars.githubusercontent.com/u/98358870?v=4',
+    gitHubLink: 'https://github.com/Immayou',
+    linkedInLink: 'https://www.linkedin.com/in/yuliya-zozulya/',
+  },
+  {
+    devName: 'Марина Петренко',
+    devPhoto: 'https://avatars.githubusercontent.com/u/99042644?v=4',
+    gitHubLink: 'https://github.com/MariOkner',
+    linkedInLink: 'https://www.linkedin.com/in/marinapetrenko/',
+  },
+  {
+    devName: 'Юлія Дольнікова',
+    devPhoto: 'https://avatars.githubusercontent.com/u/98531699?v=4',
+    gitHubLink: 'https://github.com/Dolnikova',
+    linkedInLink: 'https://www.linkedin.com/in/dolnikova-yuliia/',
+  },
+];
+
+export default developers;
