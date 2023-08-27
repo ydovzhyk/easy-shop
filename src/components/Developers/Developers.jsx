093eeb07e19b7e26e66f416ae55f56e623e7591f
Yuliya Zozulya <djulia@ukr.net>
2023-08-27 22:54:31 +0300

    fix

--- src/components/Developers/Developers.jsx
+++ src/components/Developers/Developers.jsx
@@ -11,24 +11,43 @@ const Developers = () => {
       <Container>
         <h1 className={s.developersHeading}>Розробники сайту</h1>
         <ul className={s.developersList}>
-          {developers.map(({ devName, devPhoto, gitHubLink, linkedInLink }) => (
-            <li className={s.listItem} key={devName}>
-              <div className={s.avatarWrapper}>
-                <img src={devPhoto} alt={`developer ${devName}`} />
-              </div>
-              <div className={s.bottomContentWrapper}>
-                <Text text={devName} textClass="cardMainText" />
-                <div className={s.bottomSocialContentWrapper}>
-                  <a href={gitHubLink} className={s.socialLink}>
-                    <BsGithub size={35} className={s.socialIcon} />
-                  </a>
-                  <a href={linkedInLink}>
-                    <BsLinkedin size={35} className={s.socialIcon} />
-                  </a>
+          {developers.map(
+            ({ devName, devPhoto, gitHubLink, linkedInLink, email }) => (
+              <li className={s.listItem} key={devName}>
+                <div className={s.avatarWrapper}>
+                  <img src={devPhoto} alt={`developer ${devName}`} />
                 </div>
-              </div>
-            </li>
-          ))}
+                <div className={s.bottomContentWrapper}>
+                  <Text text={devName} textClass="cardMainText" />
+                  <a
+                    href="mailto:"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className={s.contactLinkContent}
+                  >
+                    {email}
+                  </a>
+                  <div className={s.bottomSocialContentWrapper}>
+                    <a
+                      href={gitHubLink}
+                      target="_blank"
+                      rel="noreferrer noopener nofollow"
+                      className={s.socialLink}
+                    >
+                      <BsGithub size={35} className={s.socialIcon} />
+                    </a>
+                    <a
+                      href={linkedInLink}
+                      target="_blank"
+                      rel="noreferrer noopener nofollow"
+                    >
+                      <BsLinkedin size={35} className={s.socialIcon} />
+                    </a>
+                  </div>
+                </div>
+              </li>
+            )
+          )}
         </ul>
       </Container>
     </section>
--- src/components/Developers/developersList.js
+++ src/components/Developers/developersList.js
@@ -4,36 +4,42 @@ const developers = [
     devPhoto: 'https://avatars.githubusercontent.com/u/48929257?v=4',
     gitHubLink: 'https://github.com/ydovzhyk',
     linkedInLink: 'https://www.linkedin.com/in/yuriy-dovzhyk/',
+    email: 'ydovzhyk@gmail.com',
   },
   {
     devName: 'Наталія Гринук',
     devPhoto: 'https://avatars.githubusercontent.com/u/92783918?v=4',
     gitHubLink: 'https://github.com/NataliiaHrynchuk',
     linkedInLink: 'https://www.linkedin.com/in/nataliia-hrynchuk/',
+    email: 'natagrinchukk@gmail.com',
   },
   {
     devName: 'Катерина Клещ',
     devPhoto: 'https://avatars.githubusercontent.com/u/99125819?v=4',
     gitHubLink: 'https://github.com/kotyamur',
     linkedInLink: 'https://www.linkedin.com/in/kateryna-kleshch/',
+    email: 'katyaka1993@outlook.com',
   },
   {
     devName: 'Юлія Зозуля',
     devPhoto: 'https://avatars.githubusercontent.com/u/98358870?v=4',
     gitHubLink: 'https://github.com/Immayou',
     linkedInLink: 'https://www.linkedin.com/in/yuliya-zozulya/',
+    email: 'immayou1305@gmail.com',
   },
   {
     devName: 'Марина Петренко',
     devPhoto: 'https://avatars.githubusercontent.com/u/99042644?v=4',
     gitHubLink: 'https://github.com/MariOkner',
     linkedInLink: 'https://www.linkedin.com/in/marinapetrenko/',
+    email: 'mariokner@gmail.com',
   },
   {
     devName: 'Юлія Дольнікова',
     devPhoto: 'https://avatars.githubusercontent.com/u/98531699?v=4',
     gitHubLink: 'https://github.com/Dolnikova',
     linkedInLink: 'https://www.linkedin.com/in/dolnikova-yuliia/',
+    email: 'dolnikova80@gmail.com',
   },
 ];
 
