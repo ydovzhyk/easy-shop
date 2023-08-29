fd2bff044c956c8dc13b0dd5d91f4e07e4f46e47
Yuriy Dovzhyk <48929257+ydovzhyk@users.noreply.github.com>
2023-08-29 12:39:24 +0300

    Update Products.jsx

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -48,7 +48,7 @@ const Products = () => {
   const { category, subcategory } = useParams();
   const [searchParams, setSearchParams] = useSearchParams();
   const { pathname, search } = useLocation();
-  const page = searchParams.get('page');
+  // const page = searchParams.get('page');
 
   const products = useSelector(getProductsByQuery);
   const isFilterFormSubmitted = useSelector(getFilterForm);
