40f0a49eb6b73f347b5c116f2420dabb1d1e2bde
Yuriy Dovzhyk <48929257+ydovzhyk@users.noreply.github.com>
2023-08-29 11:46:42 +0300

    Update SelectedSearches.jsx

--- src/components/Favorites/SelectedSearches/SelectedSearches.jsx
+++ src/components/Favorites/SelectedSearches/SelectedSearches.jsx
@@ -1,6 +1,4 @@
-// import { useDispatch, useSelector } from 'react-redux';
 import Pagination from 'components/Shared/Pagination/Pagination';
-import Text from 'components/Shared/Text/Text';
 import Button from 'components/Shared/Button/Button';
 import RoundButton from 'components/Shared/RoundButton/RoundButton';
 import { BsTrash } from 'react-icons/bs';
