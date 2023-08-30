2d3d3fbafe9d1813e42a3c68d100de39fd616083
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2023-08-30 15:03:52 +0300

    Update AboutBuyerReviews.js

--- src/components/SellerInfo/SellerReviews/AboutBuyerReviews.js
+++ src/components/SellerInfo/SellerReviews/AboutBuyerReviews.js
@@ -1,5 +1,26 @@
+import { useSelector } from 'react-redux';
+import {
+  getLoadingReviews,
+  selectUserReviews,
+} from 'redux/review/review-selectors';
+
 const AboutBuyerReviews = () => {
-  return <div>Відгуки про мене, як про покупця</div>;
+  const sellerReviews = useSelector(selectUserReviews);
+  console.log('sellerReviews in AboutBuyerReviews:', sellerReviews);
+  const loading = useSelector(getLoadingReviews);
+
+  return (
+    <>
+      {!loading && sellerReviews.length === 0 && (
+        <p>Ще немає відгуків промене, як про покупця</p>
+      )}
+      {sellerReviews.length > 0 && (
+        <div>
+          `Тут буде {sellerReviews.length} відгуків про мене, як про покупця`
+        </div>
+      )}
+    </>
+  );
 };
 
 export default AboutBuyerReviews;
