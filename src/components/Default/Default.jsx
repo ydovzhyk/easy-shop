57638e845932f13a43809648502b2879c996ed36
Marina <99042644+MariOkner@users.noreply.github.com>
2023-09-01 01:52:35 +0300

    fixed styles  in Catalog

--- src/components/Default/Default.jsx
+++ src/components/Default/Default.jsx
@@ -5,10 +5,15 @@ import { getLogin } from 'redux/auth/auth-selectors';
 import Catalog from '../Catalog/Catalog';
 import Carousel from 'components/Carusel/Carusel';
 import Text from 'components/Shared/Text/Text';
+import useScreenResizing from '../../funcs&hooks/useScreenResizing';
+
 import s from './Default.module.scss';
 
 const Default = () => {
   const isUserLogin = useSelector(getLogin);
+
+  const viewPort = useScreenResizing();
+  const isMobile = viewPort.width < 768;
 
   return (
     <section className={s.default}>
@@ -20,7 +25,11 @@ const Default = () => {
           />
         </div>
       )}
-      <Carousel />
+      {!isMobile && (
+        <>
+          <Carousel />
+        </>
+      )}
       <Catalog />
     </section>
   );
