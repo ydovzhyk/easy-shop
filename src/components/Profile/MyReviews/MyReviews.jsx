d083ea4cbd8ae144bfb901ec5def1be1f841dc5b
Kateryna Kleshch <ketrins1993@gmail.com>
2023-09-01 16:08:20 +0300

    Update MyReviews.jsx

--- src/components/Profile/MyReviews/MyReviews.jsx
+++ src/components/Profile/MyReviews/MyReviews.jsx
@@ -1,11 +1,11 @@
-// import { useEffect, useState } from 'react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+// import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useSearchParams } from 'react-router-dom';
 import { useMediaQuery } from 'react-responsive';
 
 import { getLoadingReviews, selectUserFeedback, selectUserReviews } from 'redux/review/review-selectors';
-import { deleteReviewById, getUserFeedback, getUserReviews } from 'redux/review/review-operations';
+import { getUserFeedback, getUserReviews } from 'redux/review/review-operations';
 import { getID, selectFeedback, selectReviews } from 'redux/auth/auth-selectors';
 import { clearReviewAndFeedback } from 'redux/review/review-slice';
 
@@ -13,9 +13,10 @@ import Select from 'react-select';
 import Avatar from 'components/Profile/Avatar/Avatar';
 import StarsList from 'components/Shared/StarsList/StarsList';
 import RoundButton from 'components/Shared/RoundButton/RoundButton';
-import { BsTrash } from 'react-icons/bs';
-import { updateUserFunc } from 'funcs&hooks/updateUser';
+import { BsPencil } from 'react-icons/bs';
+// import { updateUserFunc } from 'funcs&hooks/updateUser';
 import s from './MyReviews.module.scss';
+import EditFeedbackWindow from 'components/Shared/EditFeedbackWindow/EditFeedbackWindow';
 
 const MyReviews = () => {
   const dispatch = useDispatch();
@@ -35,6 +36,8 @@ const MyReviews = () => {
     
   // const [currentSelector, setСurrentSelector] = useState('asSeller');
   // console.log(currentSelector);
+  const [isEditFeedbackWindowOpen, setIsEditFeedbackWindowOpen] = useState(false);
+  const [reviewToFeedbackWindow, setReviewToFeedbackWindow] = useState({});
   const [searchParams, setSearchParams] = useSearchParams();
   // console.log(searchParams);
   const feedbackTypeParams = searchParams.get('type');
@@ -58,11 +61,25 @@ const MyReviews = () => {
   };
   const review = feedbackTypeParams === 'asUser' ? myReview : myFeedback;
   
-  const handleButtonTrashClick = async id => {
-    await dispatch(deleteReviewById(id));
-    dispatch(getUserReviews({ userId }));
-    updateUserFunc(dispatch);
+  // const handleButtonTrashClick = async id => {
+  //   await dispatch(deleteReviewById(id));
+  //   dispatch(getUserReviews({ userId }));
+  //   updateUserFunc(dispatch);
+  // };
+
+  const toggleIsOpen = (id, rating, feedback) => {
+    if (id) {
+      setReviewToFeedbackWindow({ id, rating, feedback });
+      setIsEditFeedbackWindowOpen(!isEditFeedbackWindowOpen);
+      return;
+    }
+    setReviewToFeedbackWindow({});
+    setIsEditFeedbackWindowOpen(!isEditFeedbackWindowOpen);
   };
+
+  useEffect(() => {
+    document.body.style.overflow = isEditFeedbackWindowOpen ? 'hidden' : 'unset';
+  }, [isEditFeedbackWindowOpen]);
 
   return (
     <div className={s.reviewsPageWrapper}>
@@ -165,13 +182,25 @@ const MyReviews = () => {
                   </div>
                   {feedbackTypeParams === 'asUser' && (
                     <div className={s.buttonTrashWrapper}>
-                      <RoundButton
+                      {/* <RoundButton
                         btnClass={isTablet ? 'roundButton' : 'roundButtonMob'}
                         icon={BsTrash}
                         handleClick={handleButtonTrashClick}
                         id={_id}
+                      /> */}
+                      <RoundButton
+                        btnClass={isTablet ? 'roundButton' : 'roundButtonMob'}
+                        icon={BsPencil}
+                        handleClick={() => toggleIsOpen(_id, rating, feedback)}
+                        id={_id}
                       />
                     </div>
+                  )}
+                  {isEditFeedbackWindowOpen && (
+                    <EditFeedbackWindow
+                      hideWindow={toggleIsOpen}
+                      reviewToFeedbackWindow={reviewToFeedbackWindow}
+                    />
                   )}
                 </li>
               );
